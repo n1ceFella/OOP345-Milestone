540640a9b9ae9c866bcfa33a8c35b390325d2c6f
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2022-03-31 23:59:02 -0400

    added Utilities module prototype

---
+++ ms1/Utilities.h
@@ -0,0 +1,30 @@
+//==============================================
+// Name:           Volodymyr Labliuk
+// Student Number: 147302202
+// Email:          vlabliuk@myseneca.ca
+// Section:        NBB
+// Date:           31.03.2022
+//==============================================
+
+#ifndef SDDS_UTILITIES_H
+#define SDDS_UTILITIES_H
+#include <string>
+#include <vector>
+#include <list>
+namespace sdds
+{
+	
+	class Utilities
+	{
+	private:
+		int m_widthField{ 1 };
+		static char m_delimiter;
+		static void setDelimiter(char newDelimiter);
+		static char getDelimiter();
+	public:
+		void setFieldWidth(size_t newWidth);
+		size_t getFieldWidth() const;
+		std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+	};
+}
+#endif
